79ce61695ec517a2836784e6ac56e282e526e265
kiobu <cdolson30@gmail.com>
2021-08-28 17:46:01 -0400

    Add a few item type things.

---
+++ src/game/item/item.type.ts
@@ -0,0 +1,17 @@
+export type ItemID = string;
+export type TemplateID = ItemID;
+
+export enum Currency {
+  USD = '5696686a4bdc2da3298b456a',
+  RUB = '5449016a4bdc2d6f028b456f',
+  EUR = '569668774bdc2da2298b4568',
+}
+
+export interface ItemOfType<T> {
+  createItem<T>(): ItemOfType<T>;
+  _id: ItemID;
+  _name: string;
+  _parent: ItemID;
+  _props: Record<string, unknown>;
+  _proto: string;
+}
