01b6325cce02213bb066ac81025d881f9ee2219d
kiobu <cdolson30@gmail.com>
2021-09-04 20:41:19 -0400

    Change db/ to database/

--- src/core/database/json.database.service.ts
+++ src/core/database/json.database.service.ts
@@ -5,7 +5,7 @@ import { ShardType } from './shards/shard.types';
 @Injectable()
 export class JSONDatabaseService {
   // eslint-disable-next-line prettier/prettier
-  readonly itemsShard: JSONShard = new JSONShard(ShardType.Items, './db/items/items.json');
+  readonly itemsShard: JSONShard = new JSONShard(ShardType.Items, './database/items/items.json');
   // eslint-disable-next-line prettier/prettier
-  readonly locationsShard: JSONShard = new JSONShard(ShardType.Locations, './db/locations/');
+  readonly locationsShard: JSONShard = new JSONShard(ShardType.Locations, './database/locations/');
 }
--- src/game/item/item.module.ts
+++ src/game/item/item.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { IOModule } from 'src/core/common/util/io/io.module';
-import { DatabaseModule } from 'src/core/database/database.module';
 import { ItemController } from './item.controller';
 
 // All the game modules.
