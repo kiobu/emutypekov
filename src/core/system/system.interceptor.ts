92f11f2626e0368301d986ccdf2d3dac0cace725
kiobu <cdolson30@gmail.com>
2022-04-26 16:22:37 -0400

    Start compressing body

--- src/core/system/system.interceptor.ts
+++ src/core/system/system.interceptor.ts
@@ -5,8 +5,13 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 import { Logger } from '@nestjs/common';
 import { Response } from 'express';
+import * as zlib from 'zlib'
+import * as util from 'util'
+
+const _deflate = util.promisify(zlib.deflate);
 
 @Injectable()
 export class SystemInterceptor implements NestInterceptor {
@@ -20,12 +25,15 @@ export class SystemInterceptor implements NestInterceptor {
       }`,
     );
 
-    const response: Response = context.switchToHttp().getResponse();
-
     if (!req.headers['x-no-compression']) {
-      response.setHeader('Content-Encoding', 'deflate');
+      //response.setHeader('Content-Encoding', 'deflate');
     }
 
-    return next.handle(); //.pipe();
+    return next.handle().pipe(map(data => {
+      const response: Response = context.switchToHttp().getResponse();
+
+      // return _deflate(data).then(data);
+      return data;
+    }))
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,6 @@ async function bootstrap(logger: Logger) {
 
   const app = await NestFactory.create<NestExpressApplication>(CoreModule);
 
-  app.use(compression({ encodings: ['deflate'], level: 9 }));
   app.useGlobalInterceptors(new SystemInterceptor());
   app.useStaticAssets(join(__dirname, '..', 'web', 'static'));
   app.setBaseViewsDir(join(__dirname, '..', 'web', 'views'));
